ee20a3723c4c6e943cb691a506a2803c45cc4db1
Lucas Shepard <lucastheshepard@gmail.com>
2020-09-20 00:56:06 -0300

    Finished clock, on to next part

--- screens/TimerScreen.tsx
+++ screens/TimerScreen.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { StyleSheet, Animated } from 'react-native';
+import { StyleSheet, Animated, Vibration} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Button } from 'react-native-elements';
+import {LinearGradient, BVLinearGradient} from 'react-native-linear-gradient';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import {Button} from 'react-native-elements'
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
 export default function TimerScreen() {
   const statesEnum = {
@@ -14,21 +15,25 @@ export default function TimerScreen() {
     LONG_BREAK: 'longBreak'
   }
 
-  const workDuration = 5;  // 25
-  const breakDuration = 1;  // 5
-  const longBreakDuration = 3;  // 15
+  const workDuration = 5;  // 25min
+  const breakDuration = 1;  // 5min
+  const longBreakDuration = 3;  // 15min
   const [isRestart, setIsRestart] = React.useState(0);
-  const [isActive, setIsActive] = React.useState(false);
+  const [isStopped, setIsStopped] = React.useState(false);
+  const [isPaused, setIsPaused] = React.useState(true);
   const [isCompleted, setCompleted] = React.useState([false, false, false, false]);
   const [clockState, setClockState] = React.useState(statesEnum.WORK);
 
   const togglePause = () => {
-      setIsActive(!isActive);
+      // setIsActive(!isPaused);
+      setIsPaused(!isPaused);
+      setIsStopped(false);
   }
 
   const stop = () => {
       setIsRestart(prevKey => prevKey + 1);
-      setIsActive(false);
+      setIsStopped(true);
+      setIsPaused(true);
   }
 
   const resetCheckboxes = () => {
@@ -78,6 +83,7 @@ export default function TimerScreen() {
     }
 
     stop();
+    // Vibration.vibrate([1000, 2000, 3000], true)
 
   }
 
@@ -92,6 +98,18 @@ export default function TimerScreen() {
       default:
         return workDuration;
     }
+  }
+
+  const pad = (num: number, size: number) => {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+  const minuteSecondsFormat = (currTime: number) => {
+    const minutes = Math.floor(currTime / 60)
+    const seconds = currTime % 60
+
+    return `${pad(minutes, 2)}:${pad(seconds, 2)}`
   }
   // const createPauseStop = () => (
     
@@ -113,20 +131,41 @@ export default function TimerScreen() {
     <View style={styles.container}>
       <CountdownCircleTimer
           key={isRestart}
-          isPlaying={isActive}
+          isPlaying={!isPaused}
           duration={ getDuration() }
+          size={360}
+          strokeWidth={24}
+          // isLinearGradient={true}
+          // gradientUniqueKey={}
           // } statesEnum.BREAK) ? { isCompleted[2]breakDuration }
           //   : workDuration}
           colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
           onComplete={timesUp}
         >
         {({ remainingTime, animatedColor }) => (
-          <Animated.Text style={{color: animatedColor}}>
-            {remainingTime}
-          </Animated.Text>
+          <View style={{flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'}}>
+            <Text></Text>
+            <Animated.Text style={{color: animatedColor, fontSize: 64, marginTop: 0}}>
+              {minuteSecondsFormat(remainingTime)}
+            </Animated.Text>
+            {(() => {
+                if (clockState === statesEnum.BREAK) {
+                  return <Text style={{fontSize: 32, textAlign: 'center', marginTop: 0}}>Time to connect!</Text>
+                }
+                else if (isStopped) {
+                  return <Text style={{fontSize: 32, textAlign: 'center', marginTop: 0}}>Ready?</Text>
+                }
+                else if (clockState === statesEnum.WORK) {
+                  return <Text style={{fontSize: 32, textAlign: 'center', marginTop: 0}}>GRIND TIME</Text>
+                }
+                else {
+                  return <Text style={{fontSize: 32, textAlign: 'center', marginTop: 0}}>Time to connect!</Text>
+                }
+            })()}
+          </View>
         )}
      </CountdownCircleTimer>
-     <View style={styles.rowItems}>
+     <View style={styles.checkBoxRow}>
       {(isCompleted[0]) ? <MaterialIcons size={30} style={{ marginBottom: -3 }} name="radio-button-checked" color={'black'}/>
                           : <MaterialIcons size={30} style={{ marginBottom: -3 }} name="radio-button-unchecked" color={'black'}/>}
       {(isCompleted[1]) ? <MaterialIcons size={30} style={{ marginBottom: -3 }} name="radio-button-checked" color={'black'}/>
@@ -140,13 +179,30 @@ export default function TimerScreen() {
           <View style={styles.buttonView}>
               <Button
                   onPress={togglePause}
-                  title={isActive ? 'Pause' : 'Start'}
+                  title={isPaused ? 'Start' : 'Pause'}
+                  titleStyle={{fontSize: 32, color: '#4169E1'}}
+                  containerStyle={{backgroundColor:'#FFFFFF'}}
+                  buttonStyle={{borderTopWidth: 4, borderBottomWidth: 4, borderLeftWidth: 4, borderRightWidth: 2, borderColor: '#F5F5F5', backgroundColor: '#FFFFFF'}}
+                  // ViewComponent={LinearGradient}
+                  // linearGradientProps={{
+                  //   colors: ['#FFFFFF', '#FFFFFF'],
+                  //   start: { x: 0, y: 0.5 },
+                  //   end: { x: 0, y: 0.5 },
+                  // }}
               ></Button>
           </View>
           <View style={styles.buttonView}>
-              <Button color="red"
-                  onPress={isActive ? stop : resetCheckboxes}
-                  title={isActive ? "Stop" : "Restart"}
+              <Button
+                  onPress={(isStopped) ? resetCheckboxes :  stop}
+                  title={(isStopped) ? "Restart" : "Stop" }
+                  titleStyle={{fontSize: 32, color: '#800000'}}
+                  buttonStyle={{borderTopWidth: 4, borderBottomWidth: 4, borderLeftWidth: 2,  borderRightWidth: 4, borderColor: '#F5F5F5', backgroundColor: '#FFFFFF'}}
+                  // ViewComponent={LinearGradient}
+                  // linearGradientProps={{
+                  //   colors: ['#FFFFFF', '#FFFFFF'],
+                  //   start: { x: 0, y: 0.5 },
+                  //   end: { x: 0, y: 0.5 },
+                  // }}
               ></Button>
           </View>
       </View>
@@ -178,23 +234,29 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'space-evenly',
+      justifyContent: 'flex-end',
     },
-
+  checkBoxRow: {
+    //   flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      margin: 15
+  },
   rowItems: {
   //   flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   buttonView: {
     flex: 1,
+    marginTop: 50
     // borderWidth: 1,
     // borderColor: '#DCDCDC'
   },
   button: {
       borderRadius: 0,
-      borderWidth: 10,
-      color: 'red'
+      borderWidth: 10
   }
 });
